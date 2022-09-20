3b7a81674d6aa2a1bf4945eb57750ec835182a41
Konstantyn <k.buhantsev@gmail.com>
2022-09-20 15:35:59 +0300

    Statistis

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,10 @@
 import Profile from './Profile/Profile';
 import Section from './Section/Section';
 import Container from './Container/Container';
+import Statistics from './Statistics/Statistics';
 
 import userData from '../user.json';
+import statisticsData from '../data.json';
 
 function App() {
   return (
@@ -16,7 +18,10 @@ function App() {
           stats={userData.stats}
         />
       </Section>
-      <Section title="2- Секция статистики"></Section>
+      <Section title="2- Секция статистики">
+        <Statistics title={'Upload stats'} stats={statisticsData} />
+        <Statistics stats={statisticsData} />
+      </Section>
       <Section title="3 - Список друзей"></Section>
       <Section title="4 - История транзакций"></Section>
     </Container>
